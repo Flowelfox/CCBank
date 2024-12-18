f40afde2181380b7be9f1d1f0242899292280c54
Flowelfox <Flowelfox@gmail.com>
2024-12-18 21:40:12 +0200

    Updated wallet installer to use folder

--- ccWallet/installer.lua
+++ ccWallet/installer.lua
@@ -5,6 +5,7 @@ local argStr = table.concat({...}, " ")
 
 DOWNLOADS[#DOWNLOADS + 1] = "https://raw.githubusercontent.com/Flowelfox/CCWallet/main/ccWallet/version.txt"
 DOWNLOADS[#DOWNLOADS + 1] = "https://basalt.madefor.cc/install.lua"
+DOWNLOADS[#DOWNLOADS + 1] = "https://raw.githubusercontent.com/Flowelfox/CCWallet/main/ccWallet/installer.lua"
 DOWNLOADS[#DOWNLOADS + 1] = "https://raw.githubusercontent.com/Flowelfox/CCWallet/main/ccWallet/bankAPI.lua"
 DOWNLOADS[#DOWNLOADS + 1] = "https://raw.githubusercontent.com/Flowelfox/CCWallet/main/ccWallet/wallet.lua"
 DOWNLOADS[#DOWNLOADS + 1] = "https://raw.githubusercontent.com/Flowelfox/CCWallet/main/ccWallet/sha256.lua"
@@ -14,6 +15,7 @@ local width, height = term.getSize()
 local totalDownloaded = 0
 local barLine = 6
 local line = 8
+local installFolder = "ccWallet"
 local isPocket = true
 if pocket then
     isPocket = true
@@ -56,7 +58,7 @@ local function download(path, attempt)
     end
     local data = rawData.readAll()
 
-    local file = fs.open(fileName, "w")
+    local file = fs.open(installFolder .. '/' .. fileName, "w")
     file.write(data)
     file.close()
 end
@@ -64,7 +66,7 @@ end
 local function downloadAll(downloads, total)
     local nextFile = table.remove(downloads, 1)
     if nextFile then
-        sleep(1)
+        sleep(0.5)
         parallel.waitForAll(function() downloadAll(downloads, total) end, function()
             download(nextFile, 1)
             totalDownloaded = totalDownloaded + 1
@@ -77,24 +79,25 @@ local function installBasalt()
     if fs.exists("startup") then
         fs.delete("startup")
     end
-    shell.run("install", "release", "latest.lua")
-    fs.delete("install.lua")
+    shell.run(installFolder .. "/install.lua", "release", "latest.lua")
+    shell.run("mv", "basalt.lua", installFolder .. "/basalt.lua")
+    fs.delete(installFolder .. "/install.lua")
 end
 
 local function rewriteStartup()
     local file = fs.open("startup", "w")
 
-    file.writeLine("shell.run(\"installer.lua\")")
+    file.writeLine("shell.run(\"".. installFolder .. "/installer.lua\")")
     file.writeLine("while (true) do")
-    file.writeLine("	shell.run(\"wallet.lua\")")
+    file.writeLine("	shell.run(\"" .. installFolder .. "/wallet.lua\")")
     file.writeLine("	sleep(2)")
     file.writeLine("end")
     file.close()
 end
 
 local function checkCurrentVersion()
-    if fs.exists("version.txt") then
-        local file = fs.open("version.txt", "r")
+    if fs.exists(installFolder .. "/version.txt") then
+        local file = fs.open(installFolder .. "/version.txt", "r")
         local version = file.readAll()
         file.close()
         return version
